355bcdb777d9bab9debe559fac789664ae0f4c58
varnit-ta <varnitsingh65@gmail.com>
2024-03-13 12:36:19 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	new file:   src/app/about/page.tsx
    	modified:   src/app/layout.tsx
    	modified:   src/app/products/[productId]/page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "About Me",
+}
+
+const About = () => {
+    return (  
+        <h1>About Me</h1>
+    );
+}
+ 
+export default About;
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,19 @@
-interface ProductIdInterface {
-  params: { productId: string };
+//Metadata generation for dynamix routing
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  }
 }
 
-const ProductId: React.FC<ProductIdInterface> = ({ params }) => {
+export const generateMetadata = ({params}: Props) : Metadata =>{
+  return{
+    title: `Product Id: ${params.productId}`
+  }
+}
+
+const ProductId = ({ params }: Props) => {
   return (
     <div>
       <h1>Product Id: {params.productId}</h1>
